b2c7be79c76be95db7d85a4e9a7d7493fafe5920
himanshujajoo67 <himanshujajoo67@gmail.com>
2018-02-07 19:26:48 -0500

    Added a test comment

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const argv = yargs.argv;
 var command = argv._[0];
 console.log('Command :',command);
 console.log('Yargs :',argv);
+//test comment
 
 if(command === 'add') {
 var note = notes.addNote(argv.title, argv.body);
